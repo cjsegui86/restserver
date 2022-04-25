4487cd27fd84fd7c3ac4624d69fdc5ad57a705bd
Carlos <cjsegui86@gmail.com>
2022-04-25 23:14:04 +0200

    BackEnd inicial

---
+++ routes/usuarios.routes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const { usuariosGet, 
+        usuariosPut, 
+        usuariosPost, 
+        usuariosDelete, 
+        usuariosPatch } = require('../controllers/usuarios.controller');
+
+const router = Router();
+
+  router.get('/', usuariosGet);
+
+  router.post('/', usuariosPost);
+  
+  router.put('/:id', usuariosPut);
+  
+  router.patch('/', usuariosPatch);
+  
+  router.delete('/:id', usuariosDelete);
+
+module.exports = router;
